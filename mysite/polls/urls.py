105e4b18105b6f05519dcf73fd2c2711aacc535b
Amir Khan <amirkhan4976@yahoo.com>
2023-04-17 01:27:29 +0000

    Updated files

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("printnum/<slug:guess>", views.printnum, name="printnum"),
     path("", views.index, name="index"),
 ]
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.utils.html import escape
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def printnum(request, guess):
+    response = """<html><body>
+    <p>Your input number was: """+escape(guess)+"""</p>
+    </body></html>"""
+    return HttpResponse(response)
